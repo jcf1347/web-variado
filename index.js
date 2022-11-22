7736e0f5803c4bcc559a48cc3f5a1dbf959ad4c9
jcf1347 <118777485+jcf1347@users.noreply.github.com>
2022-11-21 23:16:38 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="index.css"></link>
+      <body>
+        <buttom>   </buttom>
+        <script src="index.js"></script>
+     </body>
+
+    </head>
+</html>
